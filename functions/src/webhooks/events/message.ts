963ec4cfcd838da69cf89490ee1fcbe8939396d8
1zumisawashun <shun.1zumisawa@gmail.com>
2023-02-03 14:25:09 +0900

    templeteButtonの設置

--- functions/src/webhooks/events/message.ts
+++ functions/src/webhooks/events/message.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import { client } from "../../libs/line";
-import { WebhookEvent } from "@line/bot-sdk";
+import { WebhookEvent, TemplateMessage } from "@line/bot-sdk";
+import * as ButtonTemplate from "../../constants/buttonTemplate.json";
 
 export const message = functions.https.onRequest((req, res) => {
   const events = req.body.events;
@@ -9,6 +10,16 @@ export const message = functions.https.onRequest((req, res) => {
     if (event.type !== "message" || event.message.type !== "text") {
       return;
     }
+
+    if (event.message.text === "いでよ") {
+      const result = await client.replyMessage(
+        event.replyToken,
+        ButtonTemplate as TemplateMessage
+      );
+      res.json(result);
+      return;
+    }
+
     const result = await client.replyMessage(event.replyToken, {
       type: "text",
       text: event.message.text,
